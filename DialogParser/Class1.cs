065e60b237aa91448059bb12166c79480810b46a
Olle Logdahl <olle.logdahl@gmail.com>
2018-12-21 20:07:56 +0100

    Dialogue -> Dialog

---
+++ DialogParser/Class1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DialogParser {
+
+	public class DialogTree {
+		DialogElement[] Elements { get; set; }
+
+		public DialogTree(DialogElement[] e) {
+			Elements = e;
+		}
+
+		public DialogTree Load(string path) {
+			List<DialogElement> e = new List<DialogElement>();
+
+			StreamReader reader = new StreamReader(path);
+			string all = reader.ReadToEnd();
+
+			return new DialogTree(e.ToArray());
+		}
+	}
+
+	public class DialogElement {
+		string Text { get; set; }
+		DialogResponse[] Responses { get; set; }
+
+		public DialogElement(string s) {
+
+		}
+	}
+
+	public class DialogResponse {
+		string Text { get; set; }
+		DialogElement TargetElement { get; set; }
+
+	}
+}
